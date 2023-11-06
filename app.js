170ea79495db928d9c33470d32687777b971432c
Mari8red <mariana8oneto@gmail.com>
2023-11-06 02:09:36 -0300

    Commented code on html, css, js

--- app.js
+++ app.js
@@ -1,45 +1,41 @@
-const url = "https://meowfacts.herokuapp.com/?lang=esp"; //El link de nuestra API gatuna
-const meowButton = document.getElementById("meowButton");
+//El link de nuestra API gatuna
+const url = "https://meowfacts.herokuapp.com/?lang=esp"; 
+//Conseguimos cada Id con el DOM y los guardamos en constantes
+const meowButton1 = document.getElementById("meowButton1");
 const meowButton2 = document.getElementById("meowButton2");
 const popupMessage = document.getElementById("popupMessage"); 
 const popupContainer = document.getElementById("popupContainer");
 const staticBackdrop = document.getElementById("staticBackdrop");
-//Conseguimos cada id para luego usarlos
-
 
 
 document.addEventListener("DOMContentLoaded", () =>{
 
-    meowButton.addEventListener("click", () => {
-        console.log("Clic en meowButton");
+    //Creamos un evento click para los dos 'meowButton', que traen las curiosidades de la API haciendo una petición a través del fetch
+    meowButton1.addEventListener("click", () => {
+        console.log("meowButton1 clickeado"); //Comprobamos que funcione el botón
 
         fetch(url)
             .then(response => response.json())
             .then(data => showMeowFacts(data))
-            .catch(error => console.error("Error al mostrar meow facts", error));
+            .catch(error => console.error("Error al mostrar meow facts 1", error));
     });
     
     meowButton2.addEventListener("click", () => {
         fetch(url)
             .then(response => response.json())
-            .then(data => showMeowFacts2(data))
-            .catch(error => console.error("Error al mostrar meow facts", error));
+            .then(data => showMeowFacts(data))
+            .catch(error => console.error("Error al mostrar meow facts 2", error));
     });
     
+    //Creamos la función usada en el fetch, que toma y muestra los datos de la API en el HTML
     function showMeowFacts(meowArray) {
         if (meowArray && Array.isArray(meowArray.data)) {
             popupMessage.innerHTML = `<p  style="text-align: center;"> ${meowArray.data[0]} </p>`;
         } else {
-            console.error("Los datos recibidos no tienen el formato esperado.");
+            console.error("Los datos recibidos no se pueden mostrar");
         }
     }
     
-    function showMeowFacts2(meowArray) {
-        if (meowArray && Array.isArray(meowArray.data)) {
-            popupMessage.innerHTML = `<p  style="text-align: center;"> ${meowArray.data[0]} </p>`;
-        } else {
-            console.error("Los datos recibidos no tienen el formato esperado.");
-        }
-    }
+    //También colocamos mensajes 'console.error' por si no funcionaba alguna de las lógicas
 
 });
